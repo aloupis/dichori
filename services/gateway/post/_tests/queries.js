251a776b3cacf04326cd0206efd6051a223a208f
aloupis <manos.aloupis@gmail.com>
2021-09-11 13:39:48 +0300

    test: basic cases for post resolver

--- services/gateway/post/_tests/queries.js
+++ services/gateway/post/_tests/queries.js
@@ -1,3 +1,5 @@
+const NEW_ID = 51;
+
 const FIRST_PAGE_POSTS = `
 query POSTS_QUERY {
     posts(
@@ -21,6 +23,89 @@ query POSTS_QUERY {
   }  
 `;
 
+const ORDER_BY_POSTS = `
+query POSTS_QUERY {
+    posts(
+      offset: 0
+      limit: 10
+      orderBy: { field: "post.summary_en", direction: "asc" }
+    ) {
+      id
+      title_en
+      title_gr
+      summary_en
+      summary_gr
+      created_at
+      updated_at
+      author {
+        id
+        username
+      }
+    }
+    posts_count
+  }  
+`;
+
+const INSERT_POST = `
+mutation CREATE_POST_MUTATION {
+  insert_post(input: {
+    title_en: "Test English Title"
+    title_gr: "Test Greek Title"
+    content_en: "<p>Big English Content</p>"
+    content_gr: "<p>Big Greek Content</p>"
+    summary_en: "Short English Summary"
+    summary_gr: "Short Greek Summary"
+  }) {
+    id
+    title_en
+    title_gr
+    summary_en
+    summary_gr
+    created_at
+    updated_at
+    author {
+      id
+      username
+    }
+  }
+}
+`;
+
+const UPDATE_POST = `
+  mutation UPDATE_POST_MUTATION {
+    update_post(id: ${NEW_ID}, set: {
+      title_en: "Updated Test English Title"
+    }) {
+      id
+      title_en
+      title_gr
+      summary_en
+      summary_gr
+      created_at
+      updated_at
+      image_public_id
+      author {
+        id
+        username
+      }
+    }
+  }
+`;
+
+const DELETE_POST = `
+mutation DELETE_POST_MUTATION {
+  delete_post(id: ${NEW_ID}) {
+    success
+    message
+  }
+}
+`;
+
 module.exports = {
   FIRST_PAGE_POSTS,
+  ORDER_BY_POSTS,
+  INSERT_POST,
+  UPDATE_POST,
+  DELETE_POST,
+  NEW_ID,
 };
--- services/gateway/post/_tests/test.js
+++ services/gateway/post/_tests/test.js
@@ -2,9 +2,19 @@ const rp = require('request-promise');
 
 const API = 'http://localhost:7000/graphql';
 
-const { FIRST_PAGE_POSTS } = require('./queries');
+const {
+  FIRST_PAGE_POSTS,
+  ORDER_BY_POSTS,
+  INSERT_POST,
+  UPDATE_POST,
+  DELETE_POST,
+  NEW_ID,
+} = require('./queries');
 
-describe('Checking posts resolver', () => {
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hbm9zLmFsb3VwaXNAZ21haWwuY29tIiwiaWQiOjEsImlhdCI6MTYzMTM1MjkzN30.JEwO0lIEITdj-nyv3KbWWe6lf3CtE4HS_Y4ECRcP2HM';
+
+describe('Checking posts resolvers', () => {
   it('Retrieves the first page of posts', async () => {
     const response = await rp({
       method: 'POST',
@@ -14,5 +24,100 @@ describe('Checking posts resolver', () => {
     });
     expect(response.data.posts.length).toEqual(10);
     expect(response.data.posts_count).toEqual(50);
+  });
+
+  it('Orders post by summary_en', async () => {
+    const response = await rp({
+      method: 'POST',
+      uri: API,
+      body: { query: ORDER_BY_POSTS },
+      json: true,
+    });
+    expect(response.data.posts.length).toEqual(10);
+    expect(response.data.posts_count).toEqual(50);
+    expect(response.data.posts[0].summary_en).toEqual(
+      'Aenean fermentum. Donec ut mauris eget massa tempor convallis.'
+    );
+  });
+
+  it('Does not insert post when unauthenticated', async () => {
+    const response = await rp({
+      method: 'POST',
+      uri: API,
+      body: { query: INSERT_POST },
+      json: true,
+    });
+    expect(response.errors[0].extensions.code).toEqual('UNAUTHENTICATED');
+  });
+
+  it('Inserts post when authenticated', async () => {
+    const response = await rp({
+      method: 'POST',
+      uri: API,
+      body: { query: INSERT_POST },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      json: true,
+    });
+    expect(response.data.insert_post.id).toEqual(`${NEW_ID}`);
+    expect(response.data.insert_post.title_en).toEqual('Test English Title');
+    expect(response.data.insert_post.title_gr).toEqual('Test Greek Title');
+    expect(response.data.insert_post.summary_en).toEqual(
+      'Short English Summary'
+    );
+    expect(response.data.insert_post.summary_gr).toEqual('Short Greek Summary');
+    expect(response.data.insert_post.author.username).toEqual('a_loop_is');
+  });
+
+  it('Does not update post when unauthenticated', async () => {
+    const response = await rp({
+      method: 'POST',
+      uri: API,
+      body: { query: UPDATE_POST },
+      json: true,
+    });
+    expect(response.errors[0].extensions.code).toEqual('UNAUTHENTICATED');
+  });
+
+  it('Updates post when authenticated', async () => {
+    const response = await rp({
+      method: 'POST',
+      uri: API,
+      body: { query: UPDATE_POST },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      json: true,
+    });
+    expect(response.data.update_post.id).toEqual(`${NEW_ID}`);
+    expect(response.data.update_post.title_en).toEqual(
+      'Updated Test English Title'
+    );
+  });
+
+  it('Does not delete post when unauthenticated', async () => {
+    const response = await rp({
+      method: 'POST',
+      uri: API,
+      body: { query: DELETE_POST },
+      json: true,
+    });
+    expect(response.errors[0].extensions.code).toEqual('UNAUTHENTICATED');
+    expect(response.data.delete_post.success).toEqual(false);
+  });
+
+  it('Deletes post when authenticated', async () => {
+    const response = await rp({
+      method: 'POST',
+      uri: API,
+      body: { query: DELETE_POST },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      json: true,
+    });
+    expect(response.data.delete_post.success).toEqual(true);
+    expect(response.data.delete_post.message).toEqual(null);
   });
 });
