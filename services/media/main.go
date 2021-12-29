666961cc57304b744cf168937c692611ab45b1d7
aloupis <manos.aloupis@gmail.com>
2021-12-29 12:18:46 +0200

    giving it another go

--- services/media/main.go
+++ services/media/main.go
@@ -180,9 +180,12 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*", "https://dichori-admin.herokuapp.com", "dichori-admin.herookuapp.com"},
 		AllowMethods:     []string{"GET", "HEAD", "OPTIONS", "POST", "PUT"},
-		AllowHeaders:     []string{"sentry-trace", "pragma", "authorization", "Access-Control-Allow-Headers", "Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Access-Control-Allow-Origin"},
+		AllowHeaders:     []string{"Access-Control-Allow-Origin", "sentry-trace", "pragma", "authorization", "Access-Control-Allow-Headers", "Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Access-Control-Allow-Origin"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://dichori-admin.herokuapp.com" || origin == "http://patmos-nginx.herokuapp.com" || origin == "patmos-nginx.herokuapp.com" || origin == "https://patmos-site.herokuapp.com" || origin == "http://dichori-admin.herookuapp.com" || origin == "dichori-admin.herookuapp.com"
+		},
 	}))
 
 	// CORS for https://foo.com and https://github.com origins, allowing:
